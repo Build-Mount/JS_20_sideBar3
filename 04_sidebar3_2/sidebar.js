942d6f3ee5391e7a79f8f0c7859bd972568defce
Build Mount <47172782+Build-Mount@users.noreply.github.com>
2024-09-02 18:13:00 +0900

    Add files via upload

---
+++ 04_sidebar3_2/sidebar.js
@@ -0,0 +1,54 @@
+let offset = -200;
+
+window.addEventListener("DOMContentLoaded", () => {
+    let strHtml = `\n`;
+    strHtml += `\t<img src="humbergerIcon.svg" id="menuIcon" class="menuIcon"/>\n`;
+    strHtml += `\t<div id="sideBar">\n`;
+    strHtml += `\t\t<img src="humbergerIcon.svg" id="menuIcon2" class="menuIcon"/>\n`;
+    strHtml += `\t\t<div id="menuList">\n`;
+    strHtml += `\t\t\t<div id="home" class="menuItem">ホーム</div>\n`;
+    strHtml += `\t\t\t<div id="mypage" class="menuItem">マイページ</div>\n`;
+    strHtml += `\t\t</div>\n`;
+    strHtml += `\t</div>\n`;
+    strHtml += `\t<div id="cover" class="cover"></div>\n`;
+    menuSideBar.insertAdjacentHTML("beforeend", strHtml);
+
+    document.getElementById("menuIcon").addEventListener("click", () => {
+        showSideBar();
+    });
+
+    document.getElementById("menuIcon2").addEventListener("click", () => {
+        showSideBar();
+    });
+
+    document.getElementById("home").addEventListener("click", async () => {
+        await showSideBar();
+        location.href = "index.html";
+    });
+
+    document.getElementById("mypage").addEventListener("click", async () => {
+        await showSideBar();
+        location.href = "mypage.html";
+    });
+
+    document.getElementById("cover").addEventListener("click", () => {
+        showSideBar();
+    });
+
+});
+
+
+const showSideBar = () => {
+    document.getElementById("cover").classList.toggle("coverVisible");
+    offset = -offset;
+    const animation = document.getElementById("sideBar").animate(
+        [
+            {   transform: `translateX(${offset}px)`    }
+        ],
+        {
+            duration: 300,
+            fill: "forwards"
+        },
+    );
+    return animation.finished;
+};
